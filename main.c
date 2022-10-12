ee3b0ef198d7dafcddf194f6ed189bc74ae24303
Cole <israsanfuen@gmail.com>
2022-10-12 18:21:08 +0200

    Ej2 Terminado

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int contador=0;
+    float num,acumulado=0;
+    do {
+        printf("Introduce un valor: ");
+        scanf("%f",&num);
+        if (num!=0){
+            acumulado=acumulado+num;
+            contador++;
+        }
+    } while (num!=0);
+    if (contador==0)
+        printf("El promedio es 0 (no se cargo ningun valor)");
+    else {
+        printf("El promedio de los %d valores es: %.3f",contador,acumulado/contador);
+    }
     return 0;
 }
